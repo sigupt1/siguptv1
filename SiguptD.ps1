0598e94471dfe33e91a2d4bdc706fdefad4ffc1a
sigupt1 <115449815+sigupt1@users.noreply.github.com>
2022-10-16 17:15:46 +0300

    Update SiguptD.ps1

--- SiguptD.ps1
+++ SiguptD.ps1
@@ -1,4 +1,4 @@
-$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 
 # Indirme Modülü
 
